6aaacc1cccb4a1490f84ba7d26100df2abb9f260
Federico González <alambratore@hotmail.com>
2023-10-28 02:56:52 -0300

    credit notes X

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,24 +4,9 @@ import { DashBoard } from '../componets/common';
 
 const Component = () => (
   <Stack bg="white" p={8}>
-    <Text>Martes 27/10</Text>
+    <Text>Sábado 28/10</Text>
     <Text>FRAN:</Text>
-    <Text>Está corregida la tabla Detalle de Movimientos. Ahora muestra los Clientes.</Text>
-    <Text>
-      En el Informe de Ingresos ahora no muestra comprobantes que están en cero, como me pediste.
-    </Text>
-    <Text>
-      Está hecho (o eso creo) en el backend que hacelas Notas de Crédito X. Tuve que hacer cambios
-      grandes en algunos controladores.
-    </Text>
-    <Text>
-      No llegué a hacer el frontend (avancés un poco pero está roto), por eso no lo subí. Mañana lo
-      termino.
-    </Text>
-    <Text>
-      Probando cosas encontré unos errores complicados, pero al menos sé por qué suceden. Estoy
-      viendo la mejor forma de arreglarlos.
-    </Text>
+    <Text>Ya están implementadas las Notas de Crédito X.</Text>
   </Stack>
 );
 
